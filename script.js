9f4e390b98a0d7be085edc5bb8cbc7f6def459ba
Martian0202 <harshitanst@gmail.com>
2026-04-08 00:39:58 +0530

    Milestone 3 completed - search, filter, sort added

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ const url = "https://dummyjson.com/products?limit=9000";
 const container = document.getElementById("container");
 const loading = document.getElementById("loading");
 
+const searchInput = document.getElementById("search");
+const filterSelect = document.getElementById("filter");
+const sortSelect = document.getElementById("sort");
+
+
+// Store all products
+let allProducts = [];
+
 
 // Fetch Function
 
@@ -45,6 +53,38 @@ function displayData(products) {
     container.appendChild(card);
   });
 }
+
+// Applying All (Search + Filter + Sort together)
+function applyAll() {
+  let result = [...allProducts];
+
+  //Search
+  const searchValue = searchInput.value.toLowerCase();
+  result = result.filter(item =>
+    item.title.toLowerCase().includes(searchValue)
+  );
+
+  //Filter
+  const category = filterSelect.value;
+  if (category !== "") {
+    result = result.filter(item => item.category === category);
+  }
+
+  //Sort
+  const sortValue = sortSelect.value;
+  if (sortValue === "low") {
+    result.sort((a, b) => a.price - b.price);
+  } else if (sortValue === "high") {
+    result.sort((a, b) => b.price - a.price);
+  }
+
+  displayData(result);
+}
+
+// Event listeners
+searchInput.addEventListener("input", applyAll);
+filterSelect.addEventListener("change", applyAll);
+sortSelect.addEventListener("change", applyAll);
 
 // Calling the Function
 
